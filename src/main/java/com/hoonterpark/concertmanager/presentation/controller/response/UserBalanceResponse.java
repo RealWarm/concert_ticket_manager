f5268c1b79e3766a5949359b78d7f7e416a55d85
RealWarm <wjdgns8946@naver.com>
2025-01-10 10:31:14 +0900

    UserUseCase통합테스트 작성

--- src/main/java/com/hoonterpark/concertmanager/presentation/controller/response/UserBalanceResponse.java
+++ src/main/java/com/hoonterpark/concertmanager/presentation/controller/response/UserBalanceResponse.java
@@ -3,10 +3,11 @@ package com.hoonterpark.concertmanager.presentation.controller.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
-
+@Getter
 @AllArgsConstructor
 public class UserBalanceResponse {
     private Long balance;
