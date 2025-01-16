c9f422419314f2db1630a3bcfbb17a3faaabd8df
RealWarm <wjdgns8946@naver.com>
2025-01-17 00:20:53 +0900

    결제 컨트롤러 기능개발 완료

---
+++ src/main/java/com/hoonterpark/concertmanager/application/PaymentFacade.java
@@ -0,0 +1,47 @@
+package com.hoonterpark.concertmanager.application;
+
+import com.hoonterpark.concertmanager.domain.entity.PaymentEntity;
+import com.hoonterpark.concertmanager.domain.entity.ReservationEntity;
+import com.hoonterpark.concertmanager.domain.service.*;
+import com.hoonterpark.concertmanager.presentation.controller.request.PaymentRequest;
+import com.hoonterpark.concertmanager.presentation.controller.response.PaymentResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PaymentFacade {
+    private final UserService userService;
+    private final SeatService seatService;
+    private final TokenService tokenService;
+    private final PaymentService paymentService;
+    private final ReservationService reservationService;
+
+
+    // 콘서트 결제
+    public PaymentResponse makePayment(PaymentRequest request, String token, LocalDateTime now){
+
+        // 토큰검증 및 토큰상태변환
+        tokenService.updateTokenToPaid(token, now);
+
+        // 예약내역 존재 확인 후 결제완료로 상태 변환
+        ReservationEntity reservation = reservationService.payForReservation(request.getReservationId(), now);
+
+        // 유저 잔액확인 후 잔액차감
+        userService.payment(reservation.getUserId(), reservation.getTotalPrice());
+
+        // 좌석 확인 후 결제완료로 상태 변환
+        seatService.payForSeat(reservation.getSeatId(), now);
+
+        // 결제내역 생성
+        PaymentEntity payment = paymentService.makePayment(reservation.getId(), reservation.getTotalPrice());
+
+        return new PaymentResponse(payment.getId());
+    }//makePayment
+
+}
