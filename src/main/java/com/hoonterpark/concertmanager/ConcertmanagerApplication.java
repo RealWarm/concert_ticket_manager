e10fb08b9b72e78951a2e53c60145916bb8d4f90
RealWarm <wjdgns8946@naver.com>
2025-02-06 23:47:19 +0900

    redis 캐시 사용을 위한 기본 설정

--- src/main/java/com/hoonterpark/concertmanager/ConcertmanagerApplication.java
+++ src/main/java/com/hoonterpark/concertmanager/ConcertmanagerApplication.java
@@ -2,9 +2,10 @@ package com.hoonterpark.concertmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableCaching
 @SpringBootApplication
 public class ConcertmanagerApplication {
 
