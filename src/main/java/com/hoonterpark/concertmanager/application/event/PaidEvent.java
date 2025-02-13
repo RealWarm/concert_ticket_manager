86bd918a747f27996a6c0305c4b3845336921280
[클로잇]정훈 <h2024.jung@partner.samsung.com>
2025-02-13 16:44:26 +0900

    데이터 플랫폼에 전달이 기존 로직에 영향을 안주게 개선완료

---
+++ src/main/java/com/hoonterpark/concertmanager/application/event/PaidEvent.java
@@ -0,0 +1,19 @@
+package com.hoonterpark.concertmanager.application.event;
+
+import com.hoonterpark.concertmanager.domain.entity.ReservationEntity;
+import org.springframework.context.ApplicationEvent;
+
+
+public class PaidEvent extends ApplicationEvent {
+    private final ReservationEntity reservation;
+
+    public PaidEvent(Object source, ReservationEntity reservation) {
+        super(source);
+        this.reservation = reservation;
+    }
+
+    public ReservationEntity getReservation() {
+        return reservation;
+    }
+
+}
